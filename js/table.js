f395f8531ee1ec2e125f35dc717d6961de52e1d9
Max Engel <maxengel99@gmail.com>
2017-05-23 15:41:57 -0500

    Test for xmlhttp

--- js/table.js
+++ js/table.js
@@ -1,4 +1,15 @@
 var file = "Mentorlist.txt";
-$.get(file, function(data){
-  alert(data);
-});
+var peopleArray = [];
+
+//$.get(file, function(data){
+//  alert(data);
+//});
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", file, false);
+if(xmlhttp.readyState == 4){
+  var text = xmlhttp.responseText;
+  alert(text);
+}
+
+alert(xmlhttp.readyState == 4);
