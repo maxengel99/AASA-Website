66cddcc7738652b2ef3263062410d43a29823f26
Max Engel <maxengel99@gmail.com>
2017-05-23 15:58:13 -0500

    Check number of lines fixed

--- js/table.js
+++ js/table.js
@@ -2,6 +2,17 @@ var file = "Mentorlist.txt";
 var peopleArray = [];
 
 $.get(file, function(data){
-  var lines = data.responseText.split("\n");
-  alert(lines);
+  var lines = data.responseText.split("\n"); // creates array of each lines
+  var numOfPeople = lines.length/7; // 7 = num of lines per person
+
+  alert(lines.length);
+
+  //for(var i = 0; i < numOfPeople; i++){
+  //    peopleArray[numOfPeople].name = lines[peopleIndex * 7];
+  //    peopleArray[numOfPeople].pronouns = lines[peopleIndex * 7 + 1];
+  //    peopleArray[numOfPeople].hometown = lines[peopleIndex * 7 + 2];
+  //    peopleArray[numOfPeople].study = lines[peopleIndex * 7 + 3];
+//      peopleArray[numOfPeople].involvmenet = lines[peopleIndex * 7 + 4];
+//      peopleArray[numOfPeople].contact = lines[peopleIndex * 7 + 5];
+//  }
 });
