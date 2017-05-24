5c20de6d39275c25ac98e2b7e64c360e5612fc20
Max Engel <maxengel99@gmail.com>
2017-05-24 00:58:54 -0500

    Fix person object initialization and add sort method

--- js/table.js
+++ js/table.js
@@ -12,17 +12,29 @@ $.get(file, function(data){
   }
 
   console.log(lines);
-  var numOfPeople = lines.length/6; // 6 = num of lines per person
+  var numOfPeople = (lines.length+1)/6; // 6 = num of lines per person
   alert(numOfPeople)
 
   for(var i = 0; i < numOfPeople; i++){
-      peopleArray[numOfPeople].name = lines[numOfPeople * 7];
-      peopleArray[numOfPeople].pronouns = lines[numOfPeople * 7 + 1];
-      peopleArray[numOfPeople].hometown = lines[numOfPeople * 7 + 2];
-      peopleArray[numOfPeople].study = lines[numOfPeople * 7 + 3];
-      peopleArray[numOfPeople].involvmenet = lines[numOfPeople * 7 + 4];
-      peopleArray[numOfPeople].contact = lines[numOfPeople * 7 + 5];
+      peopleArray[numOfPeople].fullname: lines[numOfPeople * 7],
+      peopleArray[numOfPeople].pronouns: lines[numOfPeople * 7 + 1],
+      peopleArray[numOfPeople].hometown: lines[numOfPeople * 7 + 2],
+      peopleArray[numOfPeople].study: lines[numOfPeople * 7 + 3],
+      peopleArray[numOfPeople].involvmenet: lines[numOfPeople * 7 + 4],
+      peopleArray[numOfPeople].contact: lines[numOfPeople * 7 + 5]
   }
+
+  peopleArray.sort(function(a,b) {
+    if(a.fullname < b.fullname) {
+      return -1;
+    }
+
+    if(a.fullname > b.fullname) {
+      return 1;
+    }
+
+    return 0;
+  });
 
   console.log(peopleArray);
 });
