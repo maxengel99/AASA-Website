c330d72cc0c8f6e312da7c0bd6d71f0062a8dcbd
Max Engel <maxengel99@gmail.com>
2017-05-24 02:43:40 -0500

    Change contact to mail image

--- js/table.js
+++ js/table.js
@@ -46,6 +46,9 @@ $.get(file, function(data){
   for(var i = 0; i < numOfPeople; i++){
     $('#mentor-table').append("<tr><td>" + peopleArray[i].fullname + "</td><td>" + peopleArray[i].pronouns + "</td><td>"
     + peopleArray[i].hometown + "</td><td>" + peopleArray[i].study + "</td><td>" + peopleArray[i].involvement + "</td><td>"
-    + peopleArray[i].contact);
+    + "<td><a href=\"malto:" + peopleArray[i].contact + "\" target=\"_blank\"><img alt=\"Contact\" src=\"img/mail-clipart.png\"></a></td>");
+
+    //<td><a href="malto:maxengel99@gmail.com" target="_blank"><img alt="Contact" src="img/mail-clipart.png"></a></td> -->
+
   }
 });
