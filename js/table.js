bd967c961003b3893f8aac3e65ca30fe443b809c
Max Engel <maxengel99@gmail.com>
2017-05-24 00:45:05 -0500

    Update divisor and add people array

--- js/table.js
+++ js/table.js
@@ -8,23 +8,21 @@ $.get(file, function(data){
     if(/^\s*$/.test(lines[i])){
       lines.splice(i,1);  // remove blank indexes
       i--;  // check same index for repeating blank lines
-      console.log('delete');
     }
-    console.log('test');
   }
 
   console.log(lines);
-  var numOfPeople = lines.length/7; // 7 = num of lines per person
+  var numOfPeople = lines.length/6; // 6 = num of lines per person
   alert(numOfPeople)
 
-  //for(var i = 0; i < numOfPeople; i++){
-  //    peopleArray[numOfPeople].name = lines[numOfPeople * 7];
-  //    peopleArray[numOfPeople].pronouns = lines[numOfPeople * 7 + 1];
-  //    peopleArray[numOfPeople].hometown = lines[numOfPeople * 7 + 2];
-  //    peopleArray[numOfPeople].study = lines[numOfPeople * 7 + 3];
-//      peopleArray[numOfPeople].involvmenet = lines[numOfPeople * 7 + 4];
-//      peopleArray[numOfPeople].contact = lines[numOfPeople * 7 + 5];
-//  }
+  for(var i = 0; i < numOfPeople; i++){
+      peopleArray[numOfPeople].name = lines[numOfPeople * 7];
+      peopleArray[numOfPeople].pronouns = lines[numOfPeople * 7 + 1];
+      peopleArray[numOfPeople].hometown = lines[numOfPeople * 7 + 2];
+      peopleArray[numOfPeople].study = lines[numOfPeople * 7 + 3];
+      peopleArray[numOfPeople].involvmenet = lines[numOfPeople * 7 + 4];
+      peopleArray[numOfPeople].contact = lines[numOfPeople * 7 + 5];
+  }
 
   console.log(peopleArray);
 });
