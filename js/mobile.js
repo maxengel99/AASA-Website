a3baae1fbdfbcd8d46f8e82ca5d481e2d1c2429b
Max Engel <maxengel99@gmail.com>
2017-08-10 13:49:53 -0500

    finish mobile template

--- js/mobile.js
+++ js/mobile.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   if(/Mobi/.test(navigator.userAgent)) {
+    $('.mb8').css({'font-size': '30px', 'margin-bottom':'13px'});
+    $('.subtitle').css({'font-size':'27px'});
+    $('.instagram').css({height: '34px'});
+    $('.email-button').css({height: '40px'});
     $('.bio-link').css({display: 'block'});
   }
 });
